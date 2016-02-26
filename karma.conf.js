63fa07298b2afabaa908fc0e32bb6ac22ff5e259
Oleg_Malyshev <olegmmm33@gmail.com>
2016-02-26 11:38:40 +0200

    karma config fix

--- karma.conf.js
+++ karma.conf.js
@@ -4,26 +4,17 @@
 module.exports = function (config) {
     config.set({
         frameworks: ['jasmine'],
-        reporters:['dots','coverage'],
+        reporters: ['dots', 'coverage'],
         files: [
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'app/*.js',
             'test/*.js'
         ],
-        exclude: [],
-        plugins: [
-            require("karma-jasmine"),
-            require("karma-phantomjs-launcher"),
-            require("karma-phantomjs2-launcher"),
-            require("phantomjs2")
-        ],
-        preprocessors: {},
         coverageReporter: {
             type: 'html',
             dir: 'build/coverage/'
         },
-        reporters: ['progress'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
